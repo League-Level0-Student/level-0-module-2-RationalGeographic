040f9a8f6a522ebbad6d2216a1dd3c84daa2b285
Retarded <league@WTS5.attlocal.net>
2017-09-30 14:29:29 -0700

    Hello
    qwertyuiop

---
+++ src/Rollercoaster.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Rollercoaster {
+	public static void main(String[] args) {
+		string we;  
+		  JOptionPane.showInputDialog("How tall are you?");
+		
+	}
+
+}
